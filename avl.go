f4d595d4f1b724b276f745df3941cfa00fb8c074
Anhgelus Morhtuuzh <william@herges.fr>
2026-02-16 17:40:58 +0100

    refactor(): use function instead of an interface

--- avl.go
+++ avl.go
@@ -1,51 +1,61 @@
 package avl
 
-// Comparable represents a comparable type.
-type Comparable interface {
-	// Compare returns 0 when current and c are the same.
-	// Returns > 0 when current is bigger than c.
-	// Returns < 0 when current is smaller than c.
-	Compare(c Comparable) int
-}
+// Compare is a function that compares two values.
+// Returns 0 if a = b, > 0 if a > b and < 0 if a < b.
+type Compare[T any] func(a, b T) int
 
 // Node is contained in an AVL.
 //
-// K is a comparable used to order nodes.
-// V is the type value stored in the node.
-type Node[V Comparable] struct {
-	Value  V
+// T should be a pointer to return nil if the value is not found.
+type Node[T any] struct {
+	Value  T
 	heigth uint
-	left   *Node[V]
-	right  *Node[V]
+	left   *Node[T]
+	right  *Node[T]
 }
 
 // AVL is a standard AVL tree containing Node.
-type AVL[V Comparable] struct {
-	root *Node[V]
+//
+// T should be a pointer to return nil if the value is not found.
+type AVL[T any] struct {
+	root    *Node[T]
+	compare Compare[T]
 }
 
 // NewAVL creates a new AVL.
-func NewAVL[V Comparable]() *AVL[V] {
-	return &AVL[V]{}
+func NewAVL[T any](cmp Compare[T]) *AVL[T] {
+	return &AVL[T]{compare: cmp}
+}
+
+// NewAVLInt creates a new AVL storing int.
+func NewAVLInt() *AVL[int] {
+	return &AVL[int]{
+		compare: func(a, b int) int { return a - b },
+	}
 }
 
 // NewNode creates a new Node.
-func NewNode[V Comparable]() *Node[V] {
-	return &Node[V]{}
+func NewNode[T any]() *Node[T] {
+	return &Node[T]{}
 }
 
 // Get returns the value associated with the key provided.
 //
-// If key is not found, it returns the default value of V.
-// In this case, it returns nil if V is a pointer.
-func (a *AVL[V]) Get(key Comparable) V {
-	var v V
+// cmp takes the checked value and returns an integer representing the order.
+// It returns 0 if it is the value, > 0 if the checked value is bigger than the expected one and < 0 else.
+//
+//	// returns value 5 if it is present
+//	a.Get(func(v int) { return a - 5 })
+//
+// If key is not found, it returns the default value of V (nil if V is a pointer).
+func (a *AVL[T]) Get(cmp func(v T) int) T {
+	var v T
 	node := a.root
 	for node != nil {
-		res := node.Value.Compare(key)
+		res := cmp(node.Value)
 		if res == 0 {
 			return node.Value
-		} else if res > 0 {
+		} else if res < 0 {
 			node = node.right
 		} else {
 			node = node.left
@@ -55,7 +65,7 @@ func (a *AVL[V]) Get(key Comparable) V {
 }
 
 // Min returns the min contained in AVL.
-func (a *AVL[V]) Min() V {
+func (a *AVL[T]) Min() T {
 	node := a.root
 	for node.left != nil {
 		node = node.left
@@ -64,7 +74,7 @@ func (a *AVL[V]) Min() V {
 }
 
 // Max returns the max contained in AVL.
-func (a *AVL[V]) Max() V {
+func (a *AVL[T]) Max() T {
 	node := a.root
 	for node.right != nil {
 		node = node.right
@@ -73,28 +83,28 @@ func (a *AVL[V]) Max() V {
 }
 
 // Insert nodes in the AVL.
-func (a *AVL[V]) Insert(nodes ...*Node[V]) *AVL[V] {
+func (a *AVL[T]) Insert(nodes ...*Node[T]) *AVL[T] {
 	for _, node := range nodes {
 		if a.root == nil {
 			a.root = node
 		} else {
-			a.root.insert(node)
+			a.root.insert(node, a.compare)
 		}
 	}
 	return a
 }
 
 // Delete nodes in the AVL.
-func (a *AVL[V]) Delete(nodes ...*Node[V]) *AVL[V] {
+func (a *AVL[T]) Delete(nodes ...*Node[T]) *AVL[T] {
 	for _, node := range nodes {
 		if a.root != nil {
-			a.root = a.root.delete(node.Key)
+			a.root = a.root.delete(node.Value, a.compare)
 		}
 	}
 	return a
 }
 
-func (n *Node[V]) updateHeight() {
+func (n *Node[T]) updateHeight() {
 	left := uint(0)
 	if n.left != nil {
 		left = n.left.heigth
@@ -106,21 +116,21 @@ func (n *Node[V]) updateHeight() {
 	n.heigth = 1 + max(left, right)
 }
 
-func (n *Node[V]) rotateLeft() *Node[V] {
+func (n *Node[T]) rotateLeft() *Node[T] {
 	newNode := n.left
 	n.left = newNode.right
 	newNode.right = n
 	return newNode
 }
 
-func (n *Node[V]) rotateRight() *Node[V] {
+func (n *Node[T]) rotateRight() *Node[T] {
 	newNode := n.right
 	n.right = newNode.left
 	newNode.left = n
 	return newNode
 }
 
-func (n *Node[V]) rotate() *Node[V] {
+func (n *Node[T]) rotate(cmp Compare[T]) *Node[T] {
 	left := 0
 	if n.left != nil {
 		left = int(n.left.heigth)
@@ -133,13 +143,13 @@ func (n *Node[V]) rotate() *Node[V] {
 	switch comp {
 	case 2:
 		l := n.left
-		if l.Value.Compare(n.Value) < 0 {
+		if cmp(l.Value, n.Value) < 0 {
 			n.left = l.rotateLeft()
 		}
 		return n.rotateRight()
 	case -2:
 		r := n.right
-		if r.Value.Compare(n.Value) < 0 {
+		if cmp(r.Value, n.Value) < 0 {
 			n.left = r.rotateRight()
 		}
 		return n.rotateLeft()
@@ -148,8 +158,8 @@ func (n *Node[V]) rotate() *Node[V] {
 	}
 }
 
-func (n *Node[V]) insert(node *Node[V]) *Node[V] {
-	comp := n.Value.Compare(node.Value)
+func (n *Node[T]) insert(node *Node[T], cmp Compare[T]) *Node[T] {
+	comp := cmp(n.Value, node.Value)
 	if comp == 0 {
 		n.Value = node.Value
 		return n
@@ -158,32 +168,32 @@ func (n *Node[V]) insert(node *Node[V]) *Node[V] {
 			node.left = node
 			return node
 		}
-		node.left = node.left.insert(node.left)
+		node.left = node.left.insert(node.left, cmp)
 	} else {
 		if node.right == nil {
 			node.right = node
 			return node
 		}
-		node.right = node.right.insert(node.right)
+		node.right = node.right.insert(node.right, cmp)
 	}
 	n.updateHeight()
-	return n.rotate()
+	return n.rotate(cmp)
 }
 
-func (n *Node[V]) delete(key Comparable) *Node[V] {
-	comp := n.Value.Compare(key)
+func (n *Node[T]) delete(key T, cmp Compare[T]) *Node[T] {
+	comp := cmp(n.Value, key)
 	res := n
 	if comp == 0 {
 		if n.right == nil {
 			if n.left == nil {
 				return nil
 			}
-			res = n.left.delete(key)
+			res = n.left.delete(key, cmp)
 		} else if n.left == nil {
 			if n.right == nil {
 				return nil
 			}
-			res = n.right.delete(key)
+			res = n.right.delete(key, cmp)
 		} else {
 			cur := n
 			for cur.left != nil {
@@ -194,7 +204,7 @@ func (n *Node[V]) delete(key Comparable) *Node[V] {
 	}
 	if res != nil {
 		res.updateHeight()
-		return res.rotate()
+		return res.rotate(cmp)
 	}
 	return res
 }
